78cb4de8e77efb7f346f0128dfcb194948dc503f
SvitlanaTimofeyeva <s.timofeyeva@dinamicka.com>
2016-11-24 08:49:45 -0800

    update

--- js/src/validation/submit_handler.js
+++ js/src/validation/submit_handler.js
@@ -109,6 +109,28 @@ module.exports = {
             } catch (err) {
                 done();
             }
+
+            function done() {
+
+                $('.stats').html('1/1');
+                $('.meter-top span').animate({
+                    width: '100%'
+                }, {
+                    duration: 500,
+                    complete: function () {
+
+                        $('.big-container').fadeOut(500, function () {
+                            $('.thank-you-screen').css({
+                                'height': '100%'
+                            });
+                            $('.thank-you-screen').animate({
+                                opacity: 1
+                            }, 700);
+                        });
+                    }
+                })
+
+            }
         
     }
 
